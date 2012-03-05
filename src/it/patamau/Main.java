cef4a6a50704c41a953e41a3537a1b5d722904bd
patamau <patamau@gmail.com>
2012-03-05 22:38:11 +0000

    added profile manager profiles added gui lists

--- src/it/patamau/Main.java
+++ src/it/patamau/Main.java
@@ -1,9 +1,11 @@
 package it.patamau;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.swing.JFileChooser;
 
+import it.patama.data.ProfileManager;
 import it.patamau.gui.GUI;
 
 public class Main {
@@ -11,7 +13,14 @@ public class Main {
 	public static String DEF_SAVE_PATH = (new JFileChooser()).getFileSystemView().getDefaultDirectory().getAbsolutePath()+File.separator+"My Games"+File.separator+"Skyrim"+File.separator+"Saves";  
 
 	public static void main(final String args[]){
-		GUI gui = new GUI();
+		ProfileManager manager = new ProfileManager();
+		try {
+			manager.load(new File("."));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		GUI gui = new GUI(manager);
 		gui.pack();
 		gui.setLocationRelativeTo(null);
 		gui.setVisible(true);
--- src/it/patamau/gui/GUI.java
+++ src/it/patamau/gui/GUI.java
@@ -1,21 +1,30 @@
 package it.patamau.gui;
 
+import it.patama.data.ProfileData;
+import it.patama.data.ProfileManager;
 import it.patama.data.SaveData;
 
 import java.awt.BorderLayout;
+import java.awt.List;
 
 import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 import javax.swing.border.BevelBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
-public class GUI extends JFrame {
+public class GUI extends JFrame implements ListSelectionListener {
 	
-	public static void showPicture(final SaveData data){
+	public static void showQuickPick(final SaveData data){
 		JFrame frame = new JFrame();
 		frame.setTitle(data.getName()+" "+data.getLevel()+" "+data.getLocation()+" "+data.getDate());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout());
 		JLabel label = new JLabel();
 		label.setIcon(data.getScreenshot());
@@ -24,13 +33,18 @@ public class GUI extends JFrame {
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
+	
+	private final ProfileManager manager;
+	private JList profileList, savesList;
+	private DefaultListModel profileListModel, savesListModel;
 
-	public GUI(){
+	public GUI(final ProfileManager manager){
 		super();
+		this.manager = manager;
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.getContentPane().setLayout(new BorderLayout());
 		this.getContentPane().add(createLeftPanel(), BorderLayout.WEST);
-		this.getContentPane().add(createRightPanel(), BorderLayout.CENTER);
+		this.getContentPane().add(createCenterPanel(), BorderLayout.CENTER);
 	}
 	
 	/**
@@ -39,7 +53,20 @@ public class GUI extends JFrame {
 	 */
 	private JPanel createLeftPanel(){
 		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
 		panel.setBorder(BorderFactory.createEtchedBorder(1));
+		profileList = new JList(new DefaultListModel());
+		profileListModel = (DefaultListModel)profileList.getModel();
+		profileListModel.addElement(" -- CURRENT --");
+		for(String name: manager.getProfileNames()){
+			profileListModel.addElement(name);
+		}
+		profileList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		profileList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		profileList.setVisibleRowCount(-1);
+		profileList.addListSelectionListener(this);
+		JScrollPane listScroller = new JScrollPane(profileList);
+		panel.add(listScroller,BorderLayout.CENTER);
 		return panel;
 	}
 	
@@ -47,9 +74,33 @@ public class GUI extends JFrame {
 	 * Shows the currently selected save game
 	 * @return
 	 */
-	private JPanel createRightPanel(){
+	private JPanel createCenterPanel(){
 		JPanel panel = new JPanel();
-		panel.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+		panel.setLayout(new BorderLayout());
+		panel.setBorder(BorderFactory.createEtchedBorder(1));
+		savesList = new JList(new DefaultListModel());
+		savesListModel = (DefaultListModel)savesList.getModel();
+		savesList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		savesList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		savesList.setVisibleRowCount(-1);
+		//savesList.addListSelectionListener(this);
+		JScrollPane listScroller = new JScrollPane(savesList);
+		panel.add(listScroller,BorderLayout.CENTER);
 		return panel;
+	}
+
+	public void valueChanged(ListSelectionEvent e) {
+		if (e.getValueIsAdjusting() == false) {
+	        if (profileList.getSelectedIndex() == -1) {
+	        	//TODO: back to default profile
+	        } else {
+	        	String name = (String)profileList.getSelectedValue();
+	        	ProfileData profile = manager.getProfile(name);
+	        	savesListModel.removeAllElements();
+	        	for(SaveData save: profile.getSaves()){
+	        		savesListModel.addElement(save);
+	        	}
+	        }
+	    }
 	}
 }
