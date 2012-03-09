0827cd601e2a86fb96cafe0a45e5c80319647055
patamau <patamau@gmail.com>
2012-03-09 17:30:32 +0000

    parser improved added misc data to savedata

--- src/it/patamau/ssm/parser/Parser.java
+++ src/it/patamau/ssm/parser/Parser.java
@@ -16,6 +16,7 @@ public class Parser {
 	private static final Logger logger = Logger.getLogger(Parser.class.getName());
 	
 	private final byte[] buffer;
+	private byte[] imgbuffer;
 	private InputStream stream;
 	
 	public static final long bytesToLong(final byte[] b, final int len){
@@ -48,16 +49,22 @@ public class Parser {
 	
 	public Parser(){
 		this.buffer = new byte[65535];
+		this.imgbuffer = new byte[0];
 	}
 	
 	public Date parseFiletime() throws IOException{
-		stream.read(buffer,0,8);
+		if(stream.read(buffer, 0, 8)<0) throw new IOException("Unexpected end of stream");
 		long ft = bytesToLong(buffer,8);
 		ft/=10000; //nanoseconds
 		Calendar c = Calendar.getInstance();
 		c.setTimeInMillis(ft);
 		c.set(Calendar.YEAR, c.get(Calendar.YEAR)-369); //MSDN starts 1601, Java starts 1970 
 		return c.getTime();
+	}
+	
+	private int parseInt8() throws IOException{
+		if(stream.read(buffer, 0, 1)<0) throw new IOException("Unexpected end of stream");
+		return buffer[0] & 0xFF;
 	}
 	
 	private int parseInt16() throws IOException{
@@ -96,9 +103,11 @@ public class Parser {
 		int shotHeight = parseInt32();
 		logger.debug("Screenshot size ",shotWidth,"x",shotHeight);
 		int imgsize = 3*shotWidth*shotHeight;
-		byte[] data = new byte[imgsize];
+		if(imgbuffer.length<imgsize){
+			imgbuffer = new byte[imgsize];
+		}
 		int len, pos = 0;
-		while((len = stream.read(data, pos, imgsize-pos))>=0){
+		while((len = stream.read(imgbuffer, pos, imgsize-pos))>=0){
 			pos+=len;
 			logger.debug("Image read status ",pos,"/",imgsize," = ",((float)pos/(float)imgsize*100f),"%");
 			if(pos>=imgsize) break;
@@ -108,7 +117,7 @@ public class Parser {
 		int i=0;
 		for(int y=0; y<shotHeight; ++y){
 			for(int x=0; x<shotWidth; ++x){
-				int color = bytesToColor(data[i++],data[i++],data[i++]);
+				int color = bytesToColor(imgbuffer[i++],imgbuffer[i++],imgbuffer[i++]);
 				img.setRGB(x, y, color);
 			}
 		}
@@ -116,7 +125,7 @@ public class Parser {
 	}
 	
 	public void parsePluginInfo() throws IOException {
-		int pluginCount = stream.read();
+		int pluginCount = parseInt8();
 		logger.debug("Plugins ",pluginCount);
 		for(int i=0; i<pluginCount; ++i){
 			String plugin = parseString();
@@ -128,14 +137,14 @@ public class Parser {
 		int count = parseInt32();
 		for(int i=0; i<count; ++i){
 			String name = parseString();
-			int category = stream.read() & 0xFF;
+			int category = parseInt8();
 			int value = parseInt32();
 			logger.debug(name," ",category," ",value);
 		}
 	}
 	
 	public int parseRefId() throws IOException {
-		int b = stream.read() & 0xFF;
+		int b = parseInt8();
 		int code = (b & 0xC0) >> 6;
 		//XXX: I don't really need to know where the RefID is pointing at since I don't have the resources
 		/*
@@ -155,21 +164,21 @@ public class Parser {
 		}
 		*/
 		int num = (b & 0x3F) << 8;
-		num |= (stream.read() & 0xFF);
+		num |= parseInt8();
 		num <<= 8;
-		num |= (stream.read() & 0xFF);
+		num |= parseInt8();
 		return num;
 	}
 	
 	public int parseVsVal() throws IOException {
 		//memo: F=1111 8=1000 C=1100 3=0011
-		int b = stream.read() & 0xFF;
+		int b = parseInt8();
 		int val = (b & 0xC0) >> 6;
 		logger.debug("vsval key is ",val);
 		int num = (b & 0x3F);
 		for(int i=0; i<val; ++i){
 			num <<= 8;
-			int a = stream.read() & 0xFF;
+			int a = parseInt8();
 			num |= a;
 		}
 		int st = Integer.numberOfLeadingZeros(num);
@@ -188,85 +197,21 @@ public class Parser {
 		}
 	}
 	
-	public void parsePlayerLocation() throws IOException {
-		//FIXME: this isn't working!!!!!	
-		/* 	
-		int _xa = parseInt32(s);
-		//1 11010100 01100000000000011111111
-		float xa = Float.intBitsToFloat(_xa);
-		int angleID = parseRefId(s);
-		float ya = Float.intBitsToFloat(parseInt32(s));
-		float za = Float.intBitsToFloat(parseInt32(s));
-		logger.debug("RefID "+Integer.toHexString(angleID));
-		int _x = parseInt32(s);
-		//0 11100111 10001110011001101000001       
-		logger.debug("X: "+Integer.toBinaryString(_x));
-		float x = Float.intBitsToFloat(_x);
-		float y = Float.intBitsToFloat(parseInt32(s));
-		int coordID = parseRefId(s);
-		logger.debug("RefID "+Integer.toHexString(coordID));
-		float z = Float.intBitsToFloat(parseInt32(s));
-		logger.debug("Player location "+x+","+y+","+z+"@"+xa+","+ya+","+za);
-		*/
-		stream.skip(30);
-	}
-	
 	private void parseGlobalData() throws IOException {
 		int type = parseInt32();
 		int length = parseInt32();
 		logger.debug("GlobalData type=",type," length=",length);
 		switch(type){
-			case 0: //Misc Stats
+			case 0:
 				parseMiscStats();
-			break;
-			case 1:
-				parsePlayerLocation();
-			break;
+				break;
 			case 3:
-				parseGlobalVariables();
-			break;
+				//parseGlobalVariables();
+				//break;
 			default:
 				logger.debug("Unsupported GlobalData type ",type);
 				stream.skip(length);
 			break;
-		}
-	}
-	
-	public void parseChangeForm() throws IOException{
-		int formID = parseRefId();
-		int changeFlags = parseInt32();
-		int _type = stream.read() & 0xFF;
-		int size = (_type & 0xC0) >> 6;
-		int type = _type & 0x3F;
-		int version = stream.read() & 0xFF;
-		int length1 = 0, length2 = 0;
-		switch(size){
-		case 0:
-			length1 = stream.read() & 0xFF;
-			length2 = stream.read() & 0xFF;
-			break;
-		case 1:
-			length1 = parseInt16();
-			length2 = parseInt16();
-			break;
-		case 2:
-			length1 = parseInt16();
-			length2 = parseInt16();
-			break;
-		default:
-			logger.debug("Undefined changeForm data length ",size);
-			break;
-		} 
-		if(type==18){
-			logger.debug("ChangeForm "+Integer.toHexString(formID)
-					," ",Integer.toBinaryString(changeFlags)
-					," ["+size+"] ",type,"x",length1,"b compressed ",length2,"b (",version,")");
-			for(int i=0; i<length1; ++i){
-				int val = stream.read() & 0xFF;
-				logger.debug("DATA[",i,"] ",val);
-			}
-		}else{
-			stream.skip(length1);
 		}
 	}
 	
@@ -298,29 +243,23 @@ public class Parser {
 		//FIXME: only load the screenshot when required to save parsing time and memory (HUGE improvements)
 		ImageIcon screenshot = parseScreenshotData();
 		
-		//XXX: I'm sorry but the following data is pretty useless except GlobalData/MiscData which I'll give support soon :)
-		//XXX: lazy tosser
-		/*
-		int formVersion = stream.read();
+		int formVersion = parseInt8();
 		logger.debug("Form version is ",formVersion);
-		int pluginInfoSize = parseInt32(stream);
+		int pluginInfoSize = parseInt32();
 		logger.debug("PluginInfo size is ",pluginInfoSize);
-		stream.skip(pluginInfoSize);
-		int changeFormCount = 0;
+		stream.skip(pluginInfoSize); //XXX: skipping useless info
+		stream.skip(25); //XXX: skipping absolute offsets
+		/*
+		int globalDataTable1Offset = 0;
 		for(int i=0; i<25; ++i){
-			int val = parseInt32(stream);
-			if(9==i) changeFormCount=val;
-			logger.debug("FileLocationTable value ",val);
+			int val = parseInt32();
+			if(3==i){
+				globalDataTable1Offset=val;
+			}			
 		}
-		for(int i=0; i<23; ++i){
-			parseGlobalData(stream);
-		}
-		logger.debug("ChangeFormCount ",changeFormCount);
-		for(int i=0; i< changeFormCount; ++i){
-			parseChangeForm(stream);
-		}
-		logger.debug("Done");
+		logger.debug("GlobalDataTable1Offset ",globalDataTable1Offset);
 		*/
+		parseGlobalData(); //XXX: only parse miscStats which is the first globaldata
 		
 		SaveData save = new SaveData();
 		save.setName(name);
